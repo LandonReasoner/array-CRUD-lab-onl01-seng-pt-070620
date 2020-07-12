b2f0bdee8f39c8e0a6ec29153efee18d7c19ab4a
Landon <reasoner.landon90@gmail.com>
2020-07-12 21:25:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,6 +14,10 @@ def add_element_to_start_of_array(array, element)
   create_an_array.unshift("wow")
 end
 
+def add_element_to_end_of_array(array, element)
+  create_an_array.push("hi")
+end
+
 def remove_element_from_end_of_array(array)
   create_an_array.pop
 end
